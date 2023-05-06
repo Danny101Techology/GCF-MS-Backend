b1b8d49e9dfb12bd77468da89ac9111cadde471c
Dane <darpera@yahoo.com>
2023-05-06 20:21:47 +0800

    configured pg db connection

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOSTNAME', '127.0.0.1'), // Update this line
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      host: env('DATABASE_HOSTNAME', 'app-3d271fbb-b6d4-4a45-a545-442090945263-do-user-13945258-0.b.db.ondigitalocean.com'), // Update this line
+      port: env.int('DATABASE_PORT', 25060),
+      database: env('DATABASE_NAME', 'gcf-ms'),
+      user: env('DATABASE_USERNAME', 'gcf-ms'),
+      password: env('DATABASE_PASSWORD', 'AVNS_PfMJrbKCQBIBvUfgR0r'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         ca: env('DATABASE_CA'),
